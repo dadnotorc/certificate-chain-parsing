b04ffab8486b527417110de4393c6970717c87ef
Steven Tu <dadnotorc@gmail.com>
2020-10-25 21:02:14 -0400

    添加注释

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ The following describes:
 2. Java environment has been configured
 
 ### Generate certificates
-1. Generate a private key with 2048-bit RSA, and a self-signed certificate (CA)
-    * For the simplicity, "-nodes" is provided so that the private key won't be encrypted
+1. Generate a private key with 2048-bit RSA, and a self-signed certificate (CA). 
+For the simplicity, "-nodes" is provided so that the private key won't be encrypted.
     ```
     openssl req -x509 -nodes -newkey rsa:2048 -keyout cakey.pem -out cacert.pem -subj "/C=CA/ST=ON/O=Some Company/OU=IT Department/CN=example.com"
     ```
@@ -35,7 +35,7 @@ The following describes:
 4. Convert PEM certificate files to PKCS#12 format
     ```
     openssl pkcs12 -export -out ca.p12 -inkey cakey.pem -in cacert.pem
-   
+
     openssl pkcs12 -export -out client.p12 -inkey clientkey.pem -in clientcert.pem -certfile cacert.pem
     ```
 
@@ -45,10 +45,21 @@ The following describes:
 
     keytool -list -keystore client.p12 -storetype PKCS12 -storepass password
     ```
+   
+6. Use keytool to create a keystore.jks repository and import the client.p12
+    ```
+    keytool -importkeystore -srckeystore client.p12 -srcstoretype PKCS12 -destkeystore keystore.jks -deststoretype JKS
+    ```
+   
+   Note that the JKS keystore uses a proprietary format. It is recommended to migrate to PKCS12 
+   which is an industry standard format using
+    ```
+    keytool -importkeystore -srckeystore keystore.jks -destkeystore keystore.jks -deststoretype pkcs12
+    ```
 
 ### Run Java program
 1. Compile and run Cert_chain_parsing.java file
     ```
-   javac Cert_chain_parsing.java
-   java Cert_chain_parsing
-   ```
+    javac Cert_chain_parsing.java
+    java Cert_chain_parsing
+    ```
--- src/Certificate_chain_parsing.java
+++ src/Certificate_chain_parsing.java
@@ -9,6 +9,20 @@ import java.security.SignatureException;
 import java.security.cert.X509Certificate;
 import java.util.Enumeration;
 
+/**
+ * Given
+ * 1. Use an open-source tool, generate two X.509 certificates. The second certificate should be signed by the first,
+ *    forming a certificate chain in PKCS12 (.p12) format.
+ * 2. Write code with the help of an open-source library,
+ *    that will print out whether a certificate is a self-signed certificate, and the fingerprint of each certificate.
+ *
+ * TODO:
+ * 1. Handle garbage p12 files gracefully
+ * 2. Distinguish between a bad cert (i.e. tamper with cacert.pem so that verification fails) and cert that is not self-signed.
+ *
+ * A tampered cert is technically not self-signed, but a better solution would treat a bad cert as an error condition.
+ * This would have required a design change, where a cert chain would need to be provided as input.
+ */
 public class Certificate_chain_parsing {
     public static void main(String[] args) throws Exception {
 
@@ -26,6 +40,8 @@ public class Certificate_chain_parsing {
                 X509Certificate cert = (X509Certificate) p12.getCertificate(alias);
                 System.out.println("Certificate fingerprint (SHA-256): " + getSHA256Fingerprint(cert));
 
+                cert.checkValidity();
+
                 try {
                     cert.verify(cert.getPublicKey());
                     System.out.println("Certificate " + file.getName() + " is self signed");
@@ -38,6 +54,7 @@ public class Certificate_chain_parsing {
     }
 
     // Use UI to select one or more PKCS#12 certificate file(s)
+
     private static File[] getSelectedFiles() {
         FileDialog dialog = new FileDialog((Frame) null, "Select PKCS#12 certificate file(s) to open", FileDialog.LOAD);
         dialog.setFilenameFilter(new FilenameFilter() {
